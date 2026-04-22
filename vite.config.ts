0d5cbbffd53223f6e35352ab9b2a0b645532b435
Thijs Wijnmaalen <thijs@wijnmaalen.name>
2026-04-22 22:06:57 +0200

    vite 8

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       formats: ['es'],
       fileName: () => 'index.js',
     },
-    rollupOptions: {
+    rolldownOptions: {
       external: [],
       output: {
         preserveModules: false,
